730ed3be092dd0744795ee2eae50ac9d1940bfda
Juan Pablo Garcia <jp_garmal@outlook.com>
2023-04-18 18:17:11 -0600

    new routes for Episodes and Locations

--- Express_HandleBars/src/routes/index.js
+++ Express_HandleBars/src/routes/index.js
@@ -7,6 +7,8 @@ router.get('/', async (req, res) => {
     const a = "Hello World"
 
     const END_POINT = "https://rickandmortyapi.com/api/character"
+    
+    const Episodes = "https://rickandmortyapi.com/api/episode"
 
     axios.get(END_POINT)
         .then(function (response) {
@@ -23,5 +25,43 @@ router.get('/', async (req, res) => {
             })
         });
 })
+
+router.get('/locations', async (req, res) => {
+    const Locations = "https://rickandmortyapi.com/api/location"
+    axios.get(Locations)
+        .then(function (response) {
+            console.log(response.data.results)
+            res.render('index.hbs', {
+                data: response.data.results, 
+            })
+        })
+        .catch(function (error) {
+            console.log(error);
+            res.render('index.hbs', {
+                error,
+                data: [], 
+            })
+        });
+})
+
+router.get('/episodes', async (req, res) => {
+    const Episodes = "https://rickandmortyapi.com/api/episode"
+    axios.get(Episodes)
+        .then(function (response) {
+            console.log(response.data.results)
+            res.render('index.hbs', {
+                data: response.data.results,
+            })
+        })
+        .catch(function (error) {
+            console.log(error);
+            res.render('index.hbs', {
+                error,
+                data: [],
+            })
+        });
+})
+
+
 
 module.exports = router
